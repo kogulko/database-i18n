16a40fadf61a23774bcaf8d7eb4e996ed149c8ca
kogulko <a.kog@rademade.com>
2017-10-31 22:43:00 +0200

    Add find_by_key helper method

--- lib/database_i18n/helpers.rb
+++ lib/database_i18n/helpers.rb
@@ -1,6 +1,4 @@
 require 'database_i18n/helpers/translations_helper'
 
 module DatabaseI18n
-  module Helpers
-  end
 end
--- lib/database_i18n/helpers/translations_helper.rb
+++ lib/database_i18n/helpers/translations_helper.rb
@@ -1,25 +1,32 @@
 module DatabaseI18n
-  module Helpers
-    module TranslationsHelper
-      def self.translations_tree
-        obj = {}
-        DatabaseI18n::Key.roots.each do |node|
-          obj[node.name] = set_node(node, obj[node.name])
-        end
-        obj
+  module TranslationsHelper
+    def self.translations_tree
+      obj = {}
+      DatabaseI18n::Key.roots.each do |node|
+        obj[node.name] = set_node(node, obj[node.name])
       end
+      obj
+    end
 
-      def self.set_node(node, obj)
-        if node.leaf? && node.value
-          obj = node.value.body
-        else
-          obj = {}
-          node.children.includes(value: [:translations]).each do |children|
-            obj[children.name] = set_node(children, obj[children.name])
-          end
+    def self.set_node(node, obj)
+      if node.leaf? && node.value
+        obj = node.value.body
+      else
+        obj = {}
+        node.children.includes(value: [:translations]).each do |children|
+          obj[children.name] = set_node(children, obj[children.name])
         end
-        obj
       end
+      obj
+    end
+
+    def self.find_by_key(translation_key, **opts)
+      keys = translation_key.split('.')
+      current = DatabaseI18n::Key.roots.find_by(name: keys.shift)
+      keys.each { |key| current = current.children.find_by(name: key) }
+      translation = current.value.body
+      opts.each { |key, value| translation.gsub!("%{#{key}}", value) }
+      translation
     end
   end
 end
